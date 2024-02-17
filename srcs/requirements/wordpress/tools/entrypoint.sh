e2bb8bc4bf4403b0ba4cce9bff7d7fa7fd4a7ebd
bru-correa <bruuh.cor@gmail.com>
2024-02-17 19:28:14 -0300

    feat: change wordpress theme

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -12,7 +12,7 @@ if [[ ! -f "/var/www/html/wp-config.php" ]]; then
 	wp-cli.phar core install --path=$WP_PATH --url=$WP_URL --title=Inception \
 		--admin_user=$WP_ADM_USER --admin_password=$WP_ADM_PASSWORD \
 		--admin_email=$WP_ADM_EMAIL --skip-email --allow-root
-	wp-cli.phar theme install https://downloads.wordpress.org/theme/retrogeek.0.6.zip \
+	wp-cli.phar theme install https://downloads.wordpress.org/theme/blocksy.2.0.25.zip \
 		--path=$WP_PATH --activate --allow-root
 	wp-cli.phar user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD \
 		--role=author --allow-root

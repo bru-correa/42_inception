7dcb01aabd633a4f9f4a835d464e1374dd92f547
bru-correa <bruuh.cor@gmail.com>
2024-02-18 11:57:54 -0300

    fix: remove .env file from repository

--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -10,8 +10,8 @@ grep bcorrea.42.fr /etc/hosts || echo "127.0.0.1 bcorrea.42.fr" >>/etc/hosts
 if [[ -f ./srcs/.env ]]; then
 	echo "Found .env file"
 else
-	echo "Downloading .env file..."
-	wget https://raw.githubusercontent.com/bru-correa/42_inception/main/srcs/.env -O ./srcs/.env
+	echo "ERROR: You need to setup the .env file in srcs folder"
+	exit 1
 fi
 
 echo "Setup complete"
